f333a7dcd3a5a95196a8ca21ccaff867c1ea2222
Morteza Hoseinzadeh <75703879+mhz88@users.noreply.github.com>
2020-12-16 14:00:20 +0330

    Update dependencies.sh

--- eval/dependencies.sh
+++ eval/dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt-get update
+
+apt-get -y install wget
 apt-get -y install git-core
 apt-get -y install numactl
 apt-get -y install build-essential
@@ -12,25 +15,27 @@ apt-get -y install cmake
 apt-get -y install python
 apt-get -y install curl
 apt-get -y install libz-dev
-apt-get -y install doxygen
-apt-get -y install libpmem-devel libpmemobj-devel libpmemobj++-devel
-apt-get -y install linux-tools-generic linux-cloud-tools-generic linux-tool
+apt-get -y install doxygen pandoc
+apt-get -y install linux-tools-generic linux-cloud-tools-generic
+
+rm -f /usr/bin/perf
+ln -s /usr/lib/linux-tools/*/perf /usr/bin/perf
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source \$HOME/.cargo/env
+source $HOME/.cargo/env
 rustup default nightly
 
 wget https://github.com/pmem/pmdk/archive/1.8.tar.gz && \
     tar -xzvf 1.8.tar.gz && rm -f 1.8.tar.gz
 cd pmdk-1.8
-cp -f ../bst/btree.c src/examples/libpmemobj/ && make -j || exit 1
-sudo make install && cd .. || exit 1
+cp -f ../bst/* src/examples/libpmemobj/ && make -j || exit 1
+make install && cd .. || exit 1
 
 wget https://github.com/pmem/libpmemobj-cpp/archive/1.8.tar.gz && \
     tar -xzvf 1.8.tar.gz && rm -f 1.8.tar.gz
 cd libpmemobj-cpp-1.8
 cp -f ../simplekv/* examples/simplekv/
 mkdir build && cd build && cmake .. && make -j || exit 1
-sudo make install && cd ../.. || exit 1
+make install && cd ../.. || exit 1
 
 exit 0
