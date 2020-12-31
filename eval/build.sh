2eee09a2fd9f7f02e08e53175dee1d001e947e5e
Morteza Hoseinzadeh <75703879+mhz88@users.noreply.github.com>
2021-01-01 02:14:13 +0330

    Update build.sh

--- eval/build.sh
+++ eval/build.sh
@@ -25,7 +25,7 @@ cmake -D CMAKE_BUILD_TYPE=Release .. && make -j
 cd $dir_path
 git clone https://github.com/jerrinsg/go-pmem.git
 cd go-pmem/src
-./all.bash
+./make.bash
 apt -y remove golang
 apt -y autoremove
 echo "export PATH=$dir_path/go-pmem/bin:\$PATH" >> ~/.profile
@@ -37,8 +37,8 @@ go build -txn btree_map.go
 go build -txn simplekv.go
 
 source $HOME/.cargo/env
-rustup update
 rustup default nightly
+rustup update
 
 ldconfig
 
