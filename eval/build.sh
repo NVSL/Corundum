96f771f518146a42009c9afe16fa50d6513a5c1e
Morteza Hoseinzadeh <75703879+mhz88@users.noreply.github.com>
2021-01-02 10:36:52 +0330

    Update build.sh

--- eval/build.sh
+++ eval/build.sh
@@ -8,6 +8,7 @@ wget https://github.com/pmem/pmdk/archive/1.8.tar.gz && \
     tar -xzvf 1.8.tar.gz && rm -f 1.8.tar.gz && cd pmdk-1.8 && \
     make -j && make install
 
+cd $dir_path/pmdk
 wget https://github.com/pmem/libpmemobj-cpp/archive/1.8.tar.gz && \
     tar -xzvf 1.8.tar.gz && rm -f 1.8.tar.gz && cd libpmemobj-cpp-1.8 && \
     mkdir -p build && cd build && cmake -D CMAKE_BUILD_TYPE=Release .. && \
