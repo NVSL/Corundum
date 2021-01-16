d07119a8f71b1c93fab18b256cb332f61ae33817
Morteza <mhoseinzadeh@eng.ucsd.edu>
2021-01-16 18:33:07 +0330

    grep: isolation

--- eval/results.sh
+++ eval/results.sh
@@ -6,6 +6,8 @@ dir_path=$(dirname $full_path)
 echo -e "\nPerformance Results"
 cat $dir_path/outputs/perf.csv | perl -pe 's/((?<=,)|(?<=^)),/ ,/g;' | column -t -s,
 
-
 echo -e "\nScalability Results"
 cat $dir_path/outputs/scale.csv | perl -pe 's/((?<=,)|(?<=^)),/ ,/g;' | column -t -s,
+
+echo -e "\nScalability Results (Isolated)"
+cat $dir_path/outputs/scalei.csv | perl -pe 's/((?<=,)|(?<=^)),/ ,/g;' | column -t -s,
