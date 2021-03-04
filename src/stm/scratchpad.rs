5c8d9a3ef46183efbf05965f14f8b0096e281cd3
Morteza <mhoseinzadeh@eng.ucsd.edu>
2021-03-03 21:04:57 -0800

    checl ptr on recovery

--- src/stm/scratchpad.rs
+++ src/stm/scratchpad.rs
@@ -1,3 +1,4 @@
+use crate::cell::VCell;
 use crate::alloc::MemPool;
 use crate::ptr::Ptr;
 use crate::{utils, ll};
@@ -5,8 +6,16 @@ use std::{mem, ptr, alloc::*};
 
 const SCRATCHPAD_SIZE: usize = 1024;
 
+struct RawPtr(*mut u8);
+
+impl Default for RawPtr {
+    fn default() -> Self {
+        Self(ptr::null_mut())
+    }
+}
+
 pub struct Scratchpad<A: MemPool> {
-    base: *mut u8,
+    base: VCell<RawPtr, A>,
     cap: usize,
     len: usize,
     pm: Ptr<Self, A>
@@ -15,9 +24,9 @@ pub struct Scratchpad<A: MemPool> {
 impl<A: MemPool> Scratchpad<A> {
     pub(crate) fn new() -> Self {
         Self {
-            base: unsafe {
+            base: VCell::new(RawPtr(unsafe {
                 alloc(Layout::from_size_align_unchecked(SCRATCHPAD_SIZE, 1))
-            },
+            })),
             cap: SCRATCHPAD_SIZE,
             len: 0,
             pm: Ptr::dangling()
@@ -34,13 +43,13 @@ impl<A: MemPool> Scratchpad<A> {
         let len = 8 + 8 + size;
         if self.len + len > self.cap {
             let new_cap = self.cap + SCRATCHPAD_SIZE;
-            self.base = realloc(self.base,
+            self.base = VCell::new(RawPtr(realloc(self.base.0,
                 Layout::from_size_align_unchecked(self.cap, 1),
-                new_cap);
+                new_cap)));
             self.cap = new_cap;
         }
         // First 8 bytes is org_off
-        let p = self.base.add(self.len);
+        let p = self.base.0.add(self.len);
         *utils::read::<u64>(p) = off;
 
         // Second 8 bytes is the relative distance
@@ -56,7 +65,7 @@ impl<A: MemPool> Scratchpad<A> {
     }
 
     unsafe fn apply(&mut self) {
-        let mut cur = self.base as u64 + A::start();
+        let mut cur = self.base.0 as u64 + A::start();
         while cur < self.len as u64 {
             let p = utils::read_addr::<u8>(cur + A::start()) as *mut u8;
             let org_off = *utils::read::<u64>(p);
@@ -76,7 +85,7 @@ impl<A: MemPool> Scratchpad<A> {
 
     pub(crate) unsafe fn recover(&mut self) {
         if let Some(spd) = self.pm.try_deref_mut() {
-            let base = spd.base as u64 + A::start();
+            let base = spd.base.0 as u64 + A::start();
             let spd = utils::read_addr::<Self>(base);
             spd.apply();
         }
@@ -87,14 +96,14 @@ impl<A: MemPool> Scratchpad<A> {
         let (p, off, len, z) = A::pre_alloc(size + self.len);
         let base = off + size as u64;
         let spd = Self {
-            base: base as *mut u8,
+            base: VCell::new(RawPtr(base as *mut u8)),
             cap: 0,
             len: self.len,
             pm: Ptr::dangling()
         };
         mem::forget(mem::replace(utils::read(p), spd));
         let spd = utils::read::<Self>(p);
-        ptr::copy_nonoverlapping(self.base,
+        ptr::copy_nonoverlapping(self.base.0,
             utils::read_addr(base + A::start()),
             self.len);
         A::drop_on_failure(off, len, z);
@@ -125,15 +134,17 @@ impl<A: MemPool> Scratchpad<A> {
 impl<A: MemPool> Drop for Scratchpad<A> {
     fn drop(&mut self) {
         unsafe {
-            if self.cap != 0 {
-                dealloc(
-                    self.base,
-                    Layout::from_size_align_unchecked(SCRATCHPAD_SIZE, 1)
-                );
-            } else if self.len != 0 {
-                let z = A::pre_dealloc((self.base as u64 + A::start()) as *mut u8, self.len);
-                A::log64(A::off_unchecked(&self.len), 0, z);
-                A::perform(z);
+            if !self.base.0.is_null() {
+                if self.cap != 0 {
+                    dealloc(
+                        self.base.0,
+                        Layout::from_size_align_unchecked(self.cap, 1)
+                    );
+                } else if self.len != 0 {
+                    let z = A::pre_dealloc((self.base.0 as u64 + A::start()) as *mut u8, self.len);
+                    A::log64(A::off_unchecked(&self.len), 0, z);
+                    A::perform(z);
+                }
             }
         }
     }
